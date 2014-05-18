40e63a37b4c09d483416bfdcfcc1a2cd3345c8a0
Wendy <wendy@pinterest.com>
2014-05-18 11:32:56 -0700

    Initial Commit

---
+++ SiriSimulator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SiriSimulator
+//
+//  Created by Wendy Lu on 5/18/14.
+//  Copyright (c) 2014 WLWho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
